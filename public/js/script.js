59f025555116a7d300f4b61f96dabe735341d041
AD\spresse2022 <sarah.presse2022@campus-eni.fr>
2022-07-07 15:57:29 +0200

    CSS / JS background

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,11 @@
 var totalCount = 5;
+
+
+
 function ChangeIt()
 {
     var num = Math.ceil( Math.random() * totalCount );
-    document.body.background = '../img/backgrounds/'+num+'.svg';
+    document.body.background = chemin+'/img/backgrounds/'+num+'.svg';
     document.body.style.backgroundRepeat = "no-repeat";
     document.body.style.backgroundSize = "cover";
-
 }
