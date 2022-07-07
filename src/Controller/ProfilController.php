b1012f39d3e2eb833a98ea9238a62d25ddcd3842
scornou2022 <sulian.cornou@gmail.com>
2022-07-07 10:28:26 +0200

    mon profil edit

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,16 +2,49 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
+use App\Form\ProfilType;
+use App\Repository\ParticipantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/profil', name: 'profil_')]
 class ProfilController extends AbstractController
 {
-    #[Route('', name: 'edit')]
-    public function edit(): Response
+    #[Route('/edit', name: 'edit')]
+    public function edit(ParticipantRepository $participantRepository, Request $request): Response
     {
-        return $this->render('profil/edit.html.twig');
+
+        $participant =  $this->getUser();
+
+        $participantId = $participant->getUserIdentifier();
+        //Récupération des infos du profil
+        $profil = $participantRepository->findOneBy([
+            'username' => $participantId
+        ]);
+        dump($profil);
+        dump($participantRepository);
+        dump($participantId);
+
+        //Cas d'erreur
+        if(!$profil){
+            throw $this->createNotFoundException("Erreur : Profil introuvable !");
+        }
+
+        $profilForm = $this->createForm(ProfilType::class,$profil);
+        $profilForm->handleRequest($request);
+
+        if($profilForm->isSubmitted() && $profilForm->isValid()){
+            $participantRepository->add($profil, true);
+
+            $this->addFlash("success", "Profil modifié !");
+            return $this->redirectToRoute("profil_edit");
+        }
+
+        return $this->render('profil/edit.html.twig', [
+            "profilForm" => $profilForm->createView(),
+        ]);
     }
 }
